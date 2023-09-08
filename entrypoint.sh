c53f80a83471c40c38017e28a8dcbdd944b83cfc
Damian Peckett <damian@pecke.tt>
2023-09-08 15:58:38 +0200

    feat: make k8s compatible

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
-set -e
+set -em
+
+# Don't take ownwership of the host devices.
+mount -o remount,ro /sys
+
+nsenter -t 1 -n /lib/systemd/systemd-udevd --daemon
 
 export SYSTEMD_IGNORE_CHROOT=1
 
-nsenter -t 1 -n /bin/sh -c '
-  /lib/systemd/systemd-udevd --daemon
+udevadm monitor &
+
+unshare -m sh -c '
+  mount -o remount,rw /sys
   udevadm trigger --action=add --subsystem-match=block
-  udevadm monitor
 '
+
+fg
