241cda7271dd70dd8b21fc4f7fe5dcd9bdafd827
Damian Peckett <damian@pecke.tt>
2023-09-09 00:20:40 +0200

    feat: trigger more rules

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,6 @@ mount -o remount,rw /sys
 nsenter -t 1 -n /lib/systemd/systemd-udevd --debug --daemon
 
 udevadm trigger --action=add --subsystem-match=block
+udevadm trigger --action=change --subsystem-match=block
 
 udevadm monitor
